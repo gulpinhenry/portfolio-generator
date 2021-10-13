023e6b79bb7138998736714a82e3f688e2e0ee9c
Umera <umeramalek796@gmail.com>
2021-10-13 11:15:45 -0700

    added style to the page

--- index.js
+++ index.js
@@ -4,23 +4,72 @@ const inquirer = require("inquirer");
 function writeData(data)
 {
     let output = `
+
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title> ${data.name}'s Portfolio</title>
+
     </head>
-    <body>
-        <h1 id = "name">${data.name}</h1>
-        <h2 id = "location">${data.location}</h2>
-        <h3 id = "bio">${data.bio}</h3>
-        <a id = "linkedin" href = "${data.linkedin}" target = "_blank">${data.linkedin}</a>
-        <a id = "github" href = "${data.github}" target = "_blank">${data.github}</a>
+    <body class="d-flex h-100 text-center text-white bg-dark">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+        <div>
+            <h3 class="float-md-start mb-0">${data.name}</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+              <a class="nav-link" href="${data.linkedin}" target = "_blank">LinkedIn</a>
+              <a class="nav-link" href="${data.github}" target = "_blank">GitHub</a>
+        </nav>
+        </div>
+        </header>
+
+    <main class="px-3">
+        <h1>${data.name}</h1>
+        <p class="lead">${data.bio}</p>
+        <p class="lead">${data.location}</p>
+    </main>
+   
+  <style>
+  .nav-masthead .nav-link {
+    padding: .25rem 0;
+    font-weight: 700;
+    color: red;
+    background-color: transparent;
+    border-bottom: .25rem solid transparent;
+  }
+  
+  .nav-masthead .nav-link:hover,
+  .nav-masthead .nav-link:focus {
+    border-bottom-color: red;
+  }
+  
+  .nav-masthead .nav-link + .nav-link {
+    margin-left: 1rem;
+  }
+  
+  .nav-masthead .active {
+    color: #fff;
+    border-bottom-color: #fff;
+  }
+
+  body {
+    text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+    box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+  }
+
+  .cover-container {
+  max-width: 42em;
+}
+  
+  </style>
+   
+    </div>
     </body>
     </html>
-    
     `;
 
     fs.writeFile('output.html', output, (err) =>
@@ -57,7 +106,7 @@ inquirer
   ])
   .then((response) => {
     writeData(response);
-    console.log('Success!');
+
 
   }
   );
